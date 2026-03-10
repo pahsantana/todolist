9fc3b231fd0b4b40782879f174348fdb705663cb
Paloma Santana <santanagilberto@uol.com.br>
2026-03-09 22:59:16 -0300

    fix: entities import

--- internal/domain/repository/repository.go
+++ internal/domain/repository/repository.go
@@ -1,12 +1,14 @@
-package domain
+package repository
 
-import "context"
-import "todo-list/internal/domain/entities"
+import (
+    "context"
+    "github.com/pahsantana/todolist/internal/domain/entities"
+)
 
 type TaskRepository interface {
-	Create(ctx context.Context, task *Task) error
-	FindAll(ctx context.Context, filters map[string]string) ([]Task, error)
-	FindByID(ctx context.Context, id string) (*Task, error)
-	Update(ctx context.Context, id string, task *Task) error
-	Delete(ctx context.Context, id string) error
+    Create(ctx context.Context, task *entities.Task) error
+    FindAll(ctx context.Context, filters map[string]string) ([]entities.Task, error)
+    FindByID(ctx context.Context, id string) (*entities.Task, error)
+    Update(ctx context.Context, id string, task *entities.Task) error
+    Delete(ctx context.Context, id string) error
 }
